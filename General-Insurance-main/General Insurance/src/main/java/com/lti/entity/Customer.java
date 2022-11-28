8cb28055323708f000eda7a04ec2e06dcc624908
shru100 <srushpunyarthi@gmail.com>
2022-11-28 10:54:58 +0530

    First Commit Srushti

---
+++ General-Insurance-main/General Insurance/src/main/java/com/lti/entity/Customer.java
@@ -0,0 +1,113 @@
+package com.lti.entity;
+
+import java.time.LocalDate;
+import java.util.List;
+
+import javax.persistence.CascadeType;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.NamedQuery;
+import javax.persistence.OneToMany;
+import javax.persistence.OneToOne;
+import javax.persistence.SequenceGenerator;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "CUSTOMERS")
+
+public class Customer {
+	
+	@Id
+	@SequenceGenerator(name = "id", initialValue = 1, allocationSize=1)
+	@GeneratedValue
+	@Column(name="CustId")
+	private int id;
+	
+	@Column(name = "CustName")
+	private String name;
+	
+	@Column(name = "CustMail")
+	private String emailId;
+	
+	@Column(name = "CustDOB")
+	private LocalDate dateOfBirth;
+	
+	@Column(name = "CustContactNo")
+	private long contactNo;
+	
+	@OneToOne(cascade = CascadeType.PERSIST)
+	@JoinColumn(name="AddrId")
+	private Address address;
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getEmailId() {
+		return emailId;
+	}
+
+	public void setEmailId(String emailId) {
+		this.emailId = emailId;
+	}
+
+	public LocalDate getDateOfBirth() {
+		return dateOfBirth;
+	}
+
+	public void setDateOfBirth(LocalDate dateOfBirth) {
+		this.dateOfBirth = dateOfBirth;
+	}
+
+	public long getContactNo() {
+		return contactNo;
+	}
+
+	public void setContactNo(long contactNo) {
+		this.contactNo = contactNo;
+	}
+
+	public Address getAddress() {
+		return address;
+	}
+
+	public void setAddress(Address address) {
+		this.address = address;
+	}
+
+	public Customer(int id, String name, String emailId, LocalDate dateOfBirth, long contactNo, Address address) {
+		super();
+		this.id = id;
+		this.name = name;
+		this.emailId = emailId;
+		this.dateOfBirth = dateOfBirth;
+		this.contactNo = contactNo;
+		this.address = address;
+	}
+
+	@Override
+	public String toString() {
+		return "Customer [id=" + id + ", name=" + name + ", emailId=" + emailId + ", dateOfBirth=" + dateOfBirth
+				+ ", contactNo=" + contactNo + ", address=" + address + "]";
+	}
+	
+	
+
+	
+}
---
+++ General-Insurance-main/General Insurance/src/main/java/com/lti/entity/InsurancePlan.java
@@ -0,0 +1,102 @@
+package com.lti.entity;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.SequenceGenerator;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "INSURANCE_PLAN")
+public class InsurancePlan {
+	
+	@Id
+	@SequenceGenerator(name = "insurance_id", initialValue = 1, allocationSize=1)
+	@GeneratedValue
+	@Column(name = "InsId")
+	private int id;
+	
+	@Column(name = "InsName")
+	private String name;
+	
+	@Column(name = "InsType")
+	private String type;//third party or comprehensive
+	
+	@Column(name = "InsAmount")
+	private double amount;//2000
+	
+	@Column(name = "Commission")
+	private int commission;//500
+	
+	@Column(name = "CompanyName")
+	private String companyName;
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getType() {
+		return type;
+	}
+
+	public void setType(String type) {
+		this.type = type;
+	}
+
+	public double getAmount() {
+		return amount;
+	}
+
+	public void setAmount(double amount) {
+		this.amount = amount;
+	}
+
+	public int getCommission() {
+		return commission;
+	}
+
+	public void setCommission(int commission) {
+		this.commission = commission;
+	}
+
+	public String getCompanyName() {
+		return companyName;
+	}
+
+	public void setCompanyName(String companyName) {
+		this.companyName = companyName;
+	}
+
+	public InsurancePlan(int id, String name, String type, double amount, int commission, String companyName) {
+		super();
+		this.id = id;
+		this.name = name;
+		this.type = type;
+		this.amount = amount;
+		this.commission = commission;
+		this.companyName = companyName;
+	}
+
+	@Override
+	public String toString() {
+		return "InsurancePlan [id=" + id + ", name=" + name + ", type=" + type + ", amount=" + amount + ", commission="
+				+ commission + ", companyName=" + companyName + "]";
+	}
+	
+	
+
+		
+}
